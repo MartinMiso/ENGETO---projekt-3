93721f09eaacb42c87b0d09be5be6c1b6ee8a128
mim01li <martin.miso@seznam.cz>
2025-02-27 13:33:16 +0100

    něco

--- test_scrap2.py
+++ test_scrap2.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import csv
 import requests
 from bs4 import BeautifulSoup as bs
 
@@ -24,22 +26,40 @@ for td in soup_hlavni.find_all("td"):
                 print(f"Nalezený odkaz pro {mesto}: {odkaz_mesto}")
 
 # Výsledná url pro dané město
-url_mesto = (f"https://www.volby.cz/pls/ps2017nss/{odkaz_mesto}")
-print(url_mesto)
-response = requests.get(url_mesto)
-soup_mesto = bs(response.text, "html.parser")
+url_city = (f"https://www.volby.cz/pls/ps2017nss/{odkaz_mesto}")
+print(url_city)
+response_city = requests.get(url_city)
+soup_city = bs(response_city.text, "html.parser")
 
 # Krok 4: Najdi výsledky v detailní stránce
 # Příklad: předpokládáme, že výsledky jsou v tabulce, najdeme tabulku a vytáhneme z ní data
-table = soup_mesto.find(name="table", attrs={"class": "table"})
-if table:
+#table = soup_mesto.find_all(name="table", attrs={"class": "table"})
+upravena_data = []
+for table in soup_city.find_all("table"):
+#if table:
     # Iterace přes řádky tabulky
-    rows = table.find_all("tr")
-    for row in rows:
+    rows_city = table.find_all("tr")
+    for row in rows_city:
         cells = row.find_all("td")
         # Zde podle struktury tabulky vyber správné sloupce
         data = [cell.get_text(strip=True) for cell in cells]
-        print(data)
-else:
-    print("Tabulka s výsledky nebyla nalezena.")
+        if data and len(data) >= 2 and data[0]:
+            print(data[:2])
+            upravena_data.append(data[:2])
+            #print(upravena_data)
+        else:
+            pass
+# Vytvoříme DataFrame a zadáme názvy sloupců
+df = pd.DataFrame(upravena_data, columns=["Číslo", "Obec"])
+print(df)
+
+# Data můžeme také uložit do CSV:
+df.to_csv("data_3.csv", index=False, encoding="UTF-8")
+# print(upravena_data)
+# with open("data_3.csv", "a", newline="", encoding="UTF-8") as data_csv:
+#     writer = csv.writer(data_csv).writerow(data)
+
+
+
+
 
